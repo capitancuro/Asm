3d710fcc96caf67cd6d14ec6798c36d21128d84a
capitancuro <127172825+capitancuro@users.noreply.github.com>
2024-04-28 22:40:54 -0700

    Update assign9.asm

--- assign9.asm
+++ assign9.asm
@@ -1,32 +1,58 @@
-; Program template
-
 .386
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
-DAYS = 7     ; constant representing the number of days in a week
-HOURS = 24   ; constant representing the number of hours in a day
-MINUTES = 60  ; constant representing the number of minutes in an hour
-SECONDS = 60  ; constant representing the number of seconds in a minute
+DAYS = 7     
+HOURS = 24   
+MINUTES = 60  
+SECONDS = 60  
 
 .data
 
-date_time DWORD ? ; byte 0000 stores the DAY, byte 0001 stores the HOUR, byte 0002 stores the MINUTE, and byte 0003 stores the SECOND
-num_of_iters DWORD 0  ; counts the total number of iterations
-day BYTE DAYS   ; used to restore cl to the current day value
-hour BYTE HOURS  ; used to restore cl to the current hour value
-minute BYTE MINUTES  ; used to restore cl to the current minute value
-	
+date_time DWORD ? 
+num_of_iters DWORD 0  
+day BYTE DAYS   
+hour BYTE HOURS  
+minute BYTE MINUTES  
+    
 .code
 main proc
-	
-; IMPLEMENT THIS
-; IMPLEMENT THIS
-; IMPLEMENT THIS
-; IMPLEMENT THIS
-; IMPLEMENT THIS
 
-	invoke ExitProcess,0
+    mov [date_time], -1                     
+    mov ecx, DAYS
+    _DAYS:
+        mov day, cl
+
+        mov [date_time + 1], -1              
+        mov ecx, HOURS
+        _HOURS:
+            mov hour, cl
+
+            mov [date_time + 2], -1          
+            mov ecx, MINUTES
+            _MINUTES:
+                mov minute, cl
+
+                mov [date_time + 3], -1     
+                mov ecx, SECONDS
+                _SECONDS:
+                    add [date_time + 3], 1  
+                    add num_of_iters, 1
+                loop _SECONDS
+
+                add [date_time + 2], 1      
+                mov cl, minute
+            loop _MINUTES
+
+            add [date_time + 1], 1          
+            mov cl, hour
+        loop _HOURS
+
+        add [date_time], 1                  
+        mov cl, day
+    loop _DAYS                              
+
+    invoke ExitProcess,0
 main endp
 end main
